3d1cf6b4c5f291da0a06cd8f2935b2c07cc03705
Judeeeee <miku.nezasa@wiley-corp.com>
2021-08-16 18:29:49 +0900

    真偽条件分岐をempty?メソッドに変更
    empty?メソッドを用いれば、記述量も少なく読みやすさも上がるため今回の変更を加えた

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,13 +8,12 @@ opt.on('-m')
 
 
 ### 引数指定しない場合 => 今年・今月の結果を返す
-argv_decision = ARGV.empty?
 year_month_input = opt.parse!(ARGV)
 year = year_month_input[0].to_i
 month = year_month_input[1].to_i
 today = Date.today
 
-if argv_decision == true
+if ARGV.empty?
   year = today.year
   month = today.month
 end
