b959308d7d474582dadca205a3dae8726ce2872d
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-08-07 14:15:07 +0900

    オプションなしのlsコマンドを実装した

---
+++ 04.ls/ls.rb
@@ -0,0 +1,24 @@
+def main
+  files = get_files_no_option
+  output_list(files)
+end
+
+def get_files_no_option
+  Dir.glob("*")
+end
+
+def output_list(files, max_column = 3)
+  result = []
+  column_size = (files.size - 1)/max_column + 1
+  column_size.times { |i|
+    row = []
+    max_column.times{
+      row << files[i]
+      i += column_size
+    }
+    result << row.compact.join(' ')
+  }
+  puts result
+end
+
+main
