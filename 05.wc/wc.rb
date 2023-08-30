959f1e2e0935476093e7c8825c91d8211f125f1c
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-08-30 10:18:44 +0900

    オプションが1つの場合にtotalの出力が左詰されない&オプション指定がない場合を考慮していないが、なんとか動かせる形になってきた

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,150 +1,35 @@
 # frozen_string_literal: true
 
-
-# def main
-#   opt = OptionParser.new
-#   params = {}
-#   opt.on('-l') {|v| params[:l] = v }
-#   opt.on('-w') {|v| params[:w] = v }
-#   opt.on('-c') {|v| params[:c] = v }
-#   opt.parse!(ARGV)
-#   files =  ARGV
-
-#   l_total, w_total, c_total, output = calcurate_data(files, params)
-#   if files.size > 1
-#     total_output(l_total, w_total, c_total)
-#   end
-# end
-
-# def l_and_w_option(file,file_data,l_total,w_total)
-#   #l_option
-#   line_count = file_data.count("\n")
-#   l_total << line_count
-#   #w option
-#   word_count = file_data.split(' ').size
-#   w_total << word_count
-#   output = "#{line_count} #{word_count} #{file}"
-#   return output, l_total, w_total
-# end
-
-# #オプション2つ
-# def w_and_c_option(file,file_path,file_data,w_total,c_total)
-#   #w option
-#   word_count = file_data.split(' ').size
-#   w_total << word_count
-#   #c option
-#   file_size = File.size(file_path)
-#   c_total << file_size
-#   output = "#{word_count} #{file_size} #{file}"
-#   return output, w_total, c_total
-# end
-
-# def c_and_l_option(file,file_path,file_data,c_total,l_total)
-#   #c option
-#   file_size = File.size(file_path)
-#   c_total << file_size
-#   #l_option
-#   line_count = file_data.count("\n")
-#   l_total << line_count
-#   output = "#{line_count} #{file_size} #{file}"
-#   return output, c_total, l_total
-# end
-
-# def l_and_w_and_c_option(file,file_path,file_data,l_total,w_total,c_total)
-#   #l option
-#   line_count = file_data.count("\n")
-#   l_total << line_count
-#   #w option
-#   word_count = file_data.split(' ').size
-#   w_total << word_count
-#   #c option
-#   file_size = File.size(file_path)
-#   c_total << file_size
-#   output = "#{line_count} #{word_count} #{file_size} #{file}"
-#   return output,l_total,w_total,c_total
-# end
-
-# def calcurate_data(files, params)
-#   l_total = []
-#   w_total = []
-#   c_total = []
-#   output=""
-
-#   files.each{|file|
-#     file_path = File.expand_path(file)
-#     file_data = File.read(file_path)
-
-#     #*オプションが1つの場合(ファイル1つ、複数指定の網羅)
-#     if params.size == 1
-#       if params[:l]
-#         output,l_total = l_option(file,file_data,l_total)
-#         puts output
-#       elsif params[:w]
-#         output,w_total = w_option(file,file_data,w_total)
-#         puts output
-#       elsif params[:c]
-#         output,c_total = c_option(file,file_path,c_total)
-#         puts output
-#       end
-#     end
-
-#     #*オプションが2つの場合
-#     if params.size == 2
-#       if params.keys == [:l, :w] || params.keys == [:w, :l]
-#         output,l_total,w_total = l_and_w_option(file,file_data,l_total,w_total)
-#         puts output
-#       end
-
-#       if params.keys == [:w, :c] || params.keys == [:c, :w]
-#         output,w_total,c_total = w_and_c_option(file,file_path,file_data,w_total,c_total)
-#         puts output
-#       end
-
-#       if params.keys == [:c, :l] || params.keys == [:l, :c]
-#         output,c_total,l_total = c_and_l_option(file,file_path,file_data,c_total,l_total)
-#         puts output
-#       end
-#     end
-
-#       #* オプション指定がない、またはオプション指定が3つの場合
-#       if params.size == 0 || params.size == 3
-#         output,l_total,w_total,c_total = l_and_w_and_c_option(file,file_path,file_data,l_total,w_total,c_total)
-#         puts output
-#       end
-#   }
-#   return l_total, w_total, c_total, output
-# end
-
 require 'optparse'
 
-def l_option(file,file_data,l_total)
+def l_option(file_data,l_total)
   line_count = file_data.count("\n")
-  output = "#{line_count} #{file}"
   l_total << line_count
-  return output,l_total
+  return line_count,l_total
 end
 
-def w_option(file,file_data,w_total)
+def w_option(file_data,w_total)
   word_count = file_data.split(' ').size
-  output = "#{word_count} #{file}"
   w_total << word_count
-  return output,w_total
+  return word_count,w_total
 end
 
-def c_option(file,file_path,c_total)
+def c_option(file_path,c_total)
   file_size = File.size(file_path)
-  output = "#{file_size} #{file}"
   c_total << file_size
-  return output,c_total
+  return file_size,c_total
 end
 
 def total_output(l_total, w_total, c_total)
   #? 合計を算出する
-  l_output = l_total.sum if  l_total.sum != 0
-  w_output = w_total.sum if   w_total.sum != 0
-  c_output = c_total.sum if  c_total.sum != 0
-  puts "#{l_output}" + "#{w_output}" + "#{c_output}"  + " total"
+  l_output = l_total.sum.to_s if l_total.sum != 0
+  w_output = w_total.sum.to_s if w_total.sum != 0
+  c_output = c_total.sum.to_s if c_total.sum != 0
+  #オプションが1つだと、左詰にならない。（nilが空文字判定なので)。
+  #文字列にして足せばこの問題は解決するのだが、文字列同士の足し算になってしまう。
+  puts [l_output,w_output,c_output].join(' ')  + " total"
 end
+
 
 def main
   opt = OptionParser.new
@@ -156,31 +41,37 @@ def main
   files =  ARGV
 
   params_array = params.keys.to_a.map(&:to_s)
+  #l,w,cの順番で入れ替える
+  order = ["l","w","c"]
+  params_array = params_array.sort_by { |str| order.index(str) }
 
   l_total = []
   w_total = []
   c_total = []
   output = ""
   files.each{|file|
+    array = []
     file_path = File.expand_path(file)
     file_data = File.read(file_path)
-
     params_array.each do |param|
+
       if param == "l"
-        output,l_total = l_option(file,file_data,l_total)
-        puts output
+        line_count,l_total = l_option(file_data,l_total)
+        array << line_count
       elsif param == "w"
-        output,w_total = w_option(file,file_data,w_total)
-        puts output
+        word_count,w_total = w_option(file_data,w_total)
+        array << word_count
       elsif param == "c"
-        output,c_total =c_option(file,file_path,c_total)
-        puts output
+        file_size,c_total =c_option(file_path,c_total)
+        array << file_size
       else
-        output,l_total = l_option(file,file_data,l_total)
-        output,w_total = w_option(file,file_data,w_total)
-        output,c_total =c_option(file,file_path,c_total)
+        #オプション指定がない場合
+        output,l_total = l_option(file_data,l_total)
+        output,w_total = w_option(file_data,w_total)
+        output,c_total = c_option(file_path,c_total)
       end
     end
+    puts array.join(' ') + " " + "#{file}"
   }
 
   if files.size > 1
