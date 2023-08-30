df9f4a456fe442baf63cc71bb6a1986621137663
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-08-30 16:25:16 +0900

    可読性のために、if文の条件を入れ替えた

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -56,10 +56,10 @@ def calculate_score(frames)
     if spare?(frame)
       final_score += next_frame[0]
     elsif strike?(frame)
-      final_score += if !strike?(next_frame)
-                       next_frame[0] + next_frame.fetch(1, 0)
-                     else
+      final_score += if strike?(next_frame)
                        next_frame[0] + after_next_frame[0]
+                     else
+                       next_frame[0] + next_frame.fetch(1, 0)
                      end
     end
   end
