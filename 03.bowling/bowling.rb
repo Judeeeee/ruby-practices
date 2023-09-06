4b22be67318e81fcd793929a756bd87e945e0507
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-09-06 18:04:06 +0900

    rubocop指摘点の修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -19,11 +19,10 @@ def group_last_frame_shots(frames, last_frame, last_frame_added_shot)
   # 10フレーム目が全てストライクの場合
   if strike?(last_frame) && strike?(last_frame_added_shot)
     last_frame_final_shot = frames[11]
-    grouped_3_shots = (last_frame + last_frame_added_shot + last_frame_final_shot).reject(&:zero?)
+    (last_frame + last_frame_added_shot + last_frame_final_shot).reject(&:zero?)
   else
-    grouped_3_shots = (last_frame + last_frame_added_shot).reject(&:zero?)
+    (last_frame + last_frame_added_shot).reject(&:zero?)
   end
-  return grouped_3_shots
 end
 
 def split_by_frames(scoreboard)
@@ -33,41 +32,36 @@ def split_by_frames(scoreboard)
     score == 'X' ? [10, 0] : score.to_i
   end
 
-  frames = shots.each_slice(2).to_a
-  return frames
+  shots.each_slice(2).to_a
 end
 
 def calculate_score(frames)
-  #1~9フレーム
   final_score = 0
+
   frames[0..8].each_with_index.sum do |frame, i|
     next_frame = frames[i + 1]
-    after_next_frame = frames[i + 2]
-
-    if spare?(frame)
-      final_score += frame.sum + next_frame[0]
-    elsif strike?(frame)
-      final_score += if strike?(next_frame)
-                       frame.sum + next_frame[0] + after_next_frame[0]
-                     else
-                       frame.sum + next_frame[0..1].sum
-                     end
-    else
-      final_score += frame.sum
-    end
+    bonus = if spare?(frame)
+              next_frame[0]
+            elsif strike?(frame)
+              if strike?(next_frame)
+                after_next_frame = frames[i + 2]
+                next_frame[0] + after_next_frame[0]
+              else
+                next_frame[0..1].sum
+              end
+            else
+              0
+            end
+    final_score += frame.sum + bonus
   end
 
-  #最終フレーム
-  if frames.size == 10#最後のフレームがスペアでもストライクでもない場合
-    final_score += frames[9].sum
-  end
-  if frames.size > 10
-    last_frame = frames[9]
+  last_frame = frames[9]
+  if frames.size == 10 # 最後のフレームがスペアでもストライクでもない場合
+    final_score += last_frame.sum
+  else
     last_frame_added_shot = frames[10]
-    grouped_3_shots = group_last_frame_shots(frames, last_frame, last_frame_added_shot)
-    final_score += grouped_3_shots.sum
+    final_score += group_last_frame_shots(frames, last_frame, last_frame_added_shot).sum
   end
-  pp final_score
 end
 
 # スコア合計を標準出力。putsで引数を出力しているだけだが、テストコードで実行結果を確認するために、敢えてメソッドにしている。
