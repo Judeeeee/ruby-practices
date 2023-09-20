9df36fed893111d35f78731588d6aad28eaf4cdb
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-09-20 17:01:45 +0900

    時間が0埋めされてなかったので修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -130,7 +130,7 @@ end
 def export_timestamp(path)
   atime = File.stat(path).atime.to_s
   datetime = DateTime.parse(atime)
-  hour_minute = "#{datetime.hour}:#{datetime.minute.to_s.rjust(2, '0')}"
+  hour_minute = "#{datetime.hour.to_s.rjust(2, '0')}:#{datetime.minute.to_s.rjust(2, '0')}"
   [datetime.month.to_s.rjust(2), datetime.day.to_s.rjust(2), hour_minute].join(' ')
 end
 
