101fe38287d27d56b19635456c07920553e900bd
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-10-14 22:38:43 +0900

    RuboCop指摘箇所の修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -15,27 +15,22 @@ def c_option(file_data)
 end
 
 def total_output(l_total, w_total, c_total)
-  #? 合計を算出する
+  # ? 合計を算出する
   l_output = l_total.sum.to_s if l_total.sum != 0
   w_output = w_total.sum.to_s if w_total.sum != 0
   c_output = c_total.sum.to_s if c_total.sum != 0
-  puts [l_output,w_output,c_output,"total"].compact.join(' ')
+  puts [l_output, w_output, c_output, 'total'].compact.join(' ')
 end
-
 
 def main
   opt = OptionParser.new
   params = {}
-  opt.on('-l') {|v| params[:l] = v }
-  opt.on('-w') {|v| params[:w] = v }
-  opt.on('-c') {|v| params[:c] = v }
+  opt.on('-l') { |v| params[:l] = v }
+  opt.on('-w') { |v| params[:w] = v }
+  opt.on('-c') { |v| params[:c] = v }
   opt.parse!(ARGV)
-  files =  ARGV
+  files = ARGV
   params_array = params.keys.to_a.map(&:to_s)
-
-  order = ["l","w","c"]
-
-  params_array = params_array.sort_by { |str| order.index(str) }
 
   l_total = []
   w_total = []
@@ -46,63 +41,51 @@ def main
   input_flag = input.isatty
 
   if input_flag # lsコマンドでパイプライン処理していない場合(ややこしいので、tty?でもいいかも。)
-    files.each { |file|
-      array = []
+    files.each do |file|
+      output_line = []
       file_path = File.expand_path(file)
       file_data = File.read(file_path)
 
-      if params_array == []#オプション指定がない場合
-        line_count = l_option(file_data)
-        word_count = w_option(file_data)
-        file_size = c_option(file_data)
-        l_total << line_count
-        w_total << word_count
-        c_total << file_size
-        puts [line_count,word_count,file_size].join(' ') + " " + "#{file}"
+      if params_array == [] # オプション指定がない場合は、l,c,wどれも出力する
+        l_total << l_option(file_data)
+        w_total << w_option(file_data)
+        c_total << c_option(file_data)
+        puts [l_option(file_data), w_option(file_data), c_option(file_data), file.to_s].join(' ')
       else
         params_array.each do |param|
-          if param == "l"
-            line_count = l_option(file_data)
-            array << line_count
-            l_total << line_count
-          elsif param == "w"
-            word_count = w_option(file_data)
-            array << word_count
-            w_total << word_count
-          elsif param == "c"
-            file_size = c_option(file_data)
-            array << file_size
-            c_total << file_size
+          case param
+          when 'l'
+            output_line << l_option(file_data)
+            l_total << l_option(file_data)
+          when 'w'
+            output_line << w_option(file_data)
+            w_total << w_option(file_data)
+          when 'c'
+            output_line << c_option(file_data)
+            c_total << c_option(file_data)
           end
         end
-        puts array.join(' ') + " " + "#{file}"
+        puts "#{output_line.join(' ')} #{file}"
       end
-    }
-    if files.size > 1
-      total_output(l_total, w_total, c_total)
     end
+    total_output(l_total, w_total, c_total) if files.size > 1
   else # lsコマンドでパイプライン処理している場合
-    array = []
+    output_line = []
     file_data = $stdin.to_a.join
     if params_array == [] # オプション指定がない場合
-      line_count = l_option(file_data)
-      word_count = w_option(file_data)
-      file_size = c_option(file_data)
-      puts [line_count,word_count,file_size].join(' ')
+      puts [l_option(file_data), w_option(file_data), c_option(file_data)].join(' ')
     else
       params_array.each do |param|
-        if param == "l"
-          line_count = l_option(file_data)
-          array << line_count
-        elsif param == "w"
-          word_count = w_option(file_data)
-          array << word_count
-        elsif param == "c"
-          file_size = file_data.bytesize
-          array << file_size
+        case param
+        when 'l'
+          output_line << l_option(file_data)
+        when 'w'
+          output_line << w_option(file_data)
+        when 'c'
+          output_line << c_option(file_data)
         end
       end
-      puts array.join(' ')
+      puts output_line.join(' ')
     end
   end
 end
