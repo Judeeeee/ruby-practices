d570c6cf7422d3b78233f0487e693f29eb303b6d
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-10-27 20:37:53 +0900

    適切なメソッド名を採用してメソッドの順序を並び替えた

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -24,13 +24,6 @@ def c_option(file_data)
   file_data.bytesize
 end
 
-def calculate_total_detail_datas(file_datas)
-  file_datas.size == 1 ? calculate_total_line = file_datas.sum { |file_data| l_option(file_data) } : calculate_total_line = l_option(file_datas)
-  file_datas.size == 1 ? calculate_total_word = file_datas.sum { |file_data| w_option(file_data) } : calculate_total_word = w_option(file_datas)
-  file_datas.size == 1 ? calculate_total_bytesize = file_datas.sum { |file_data| c_option(file_data) } : calculate_total_bytesize = c_option(file_datas)
-  [calculate_total_line, calculate_total_word, calculate_total_bytesize]
-end
-
 def create_output_line_with_options(params_array, file_data)
   output_line = []
   params_array.each do |param|
@@ -47,7 +40,7 @@ def create_output_line_with_options(params_array, file_data)
 end
 
 
-def option?(files, file_datas, params_array)
+def output_detail_line(files, file_datas, params_array)
   file_datas.each_with_index do |file_data, i|
     file_name = files[i]
     if params_array.empty?
@@ -69,9 +62,7 @@ def create_end_line(params_array, total_line, total_word, total_bytesize)
   end_line
 end
 
-
-
-def add_end_line(params_array, total_line, total_word, total_bytesize)
+def determine_options_for_output_total_line(params_array, calculate_total_line, calculate_total_word, calculate_total_bytesize)
   if params_array.empty?
     "#{[total_line.to_s.rjust(8), total_word.to_s.rjust(8), total_bytesize.to_s.rjust(8)].join}"
   else
@@ -80,9 +71,11 @@ def add_end_line(params_array, total_line, total_word, total_bytesize)
   end
 end
 
-def display_total_line(file_datas)
-  total_line, total_word, total_bytesize = calculate_total_detail_datas(file_datas)
-  add_end_line(params_array, total_line, total_word, total_bytesize)
+def display_total_line(params_array, file_datas)
+  file_datas.size == 1 ? calculate_total_line = file_datas.sum { |file_data| l_option(file_data) } : calculate_total_line = l_option(file_datas)
+  file_datas.size == 1 ? calculate_total_word = file_datas.sum { |file_data| w_option(file_data) } : calculate_total_word = w_option(file_datas)
+  file_datas.size == 1 ? calculate_total_bytesize = file_datas.sum { |file_data| c_option(file_data) } : calculate_total_bytesize = c_option(file_datas)
+  determine_options_for_output_total_line(params_array, calculate_total_line, calculate_total_word, calculate_total_bytesize)
 end
 
 
@@ -97,7 +90,7 @@ def main
   input_flag = $stdin.isatty
 
   if input_flag
-    option?(files, file_datas, params_array)
+    output_detail_line(files, file_datas, params_array)
     puts display_total_line(file_datas) + "total" if files.size > 1
   else
     file_datas = $stdin.to_a.join
