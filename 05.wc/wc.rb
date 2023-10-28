35e4d6d470c3fd389445f271fed622f8f8ecde22
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-10-28 12:25:34 +0900

    オプション指定がある場合の処理をcase文を用いてまとめた

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -12,27 +12,31 @@ def define_options
   params
 end
 
-def l_option(file_data)
+def count_line(file_data)
   file_data.count("\n")
 end
 
-def w_option(file_data)
+def count_word(file_data)
   file_data.split(' ').size
 end
 
-def c_option(file_data)
+def count_bytesize(file_data)
   file_data.bytesize
 end
 
 def create_output_line_with_options(params_array, file_data)
-  params_array.map do |param|
-    case param
-    when 'l'
-      l_option(file_data).to_s.rjust(8)
-    when 'w'
-      w_option(file_data).to_s.rjust(8)
-    when 'c'
-      c_option(file_data).to_s.rjust(8)
+  if params_array.empty?
+    [count_line(file_data).to_s.rjust(8), count_word(file_data).to_s.rjust(8), count_bytesize(file_data).to_s.rjust(8)]
+  else
+    params_array.map do |param|
+      case param
+      when 'l'
+        count_line(file_data).to_s.rjust(8)
+      when 'w'
+        count_word(file_data).to_s.rjust(8)
+      when 'c'
+        count_bytesize(file_data).to_s.rjust(8)
+      end
     end
   end
 end
@@ -41,39 +45,30 @@ end
 def output_detail_line(files, file_datas, params_array)
   file_datas.each_with_index do |file_data, i|
     file_name = files[i]
-    if params_array.empty?
-      #オプションがない場合は3つ全てのデータを返す
-      puts "#{[l_option(file_data).to_s.rjust(8), w_option(file_data).to_s.rjust(8), c_option(file_data).to_s.rjust(8)].join} #{file_name}"
-    else
-      # オプションがある場合は指定されたオプションのデータを返す
-      output_line = create_output_line_with_options(params_array, file_data)
-      puts "#{output_line.join} #{file_name}"
+    output_line = create_output_line_with_options(params_array, file_data)
+    puts "#{output_line.join} #{file_name}"
+  end
+end
+
+def create_end_line(params_array,file_datas)
+  params_array.map do |param|
+    case param
+    when 'l'
+      file_datas.sum { |file_data| count_line(file_data) }.to_s.rjust(8)
+    when 'w'
+      file_datas.sum { |file_data| count_word(file_data) }.to_s.rjust(8)
+    when 'c'
+      file_datas.sum { |file_data| count_bytesize(file_data) }.to_s.rjust(8)
     end
   end
 end
 
-def create_end_line(params_array, total_line, total_word, total_bytesize)
-  end_line = []
-  end_line << total_line.to_s.rjust(8) if params_array.include?('l')
-  end_line << total_word.to_s.rjust(8) if params_array.include?('w')
-  end_line << total_bytesize.to_s.rjust(8) if params_array.include?('c')
-  end_line
-end
-
-def determine_options_for_output_total_line(params_array, calculate_total_line, calculate_total_word, calculate_total_bytesize)
-  if params_array.empty?
-    "#{[total_line.to_s.rjust(8), total_word.to_s.rjust(8), total_bytesize.to_s.rjust(8)].join}"
-  else
-    end_line = create_end_line(params_array, total_line, total_word, total_bytesize)
-    "#{end_line.join}"
-  end
-end
-
 def display_total_line(params_array, file_datas)
-  file_datas.size == 1 ? calculate_total_line = file_datas.sum { |file_data| l_option(file_data) } : calculate_total_line = l_option(file_datas)
-  file_datas.size == 1 ? calculate_total_word = file_datas.sum { |file_data| w_option(file_data) } : calculate_total_word = w_option(file_datas)
-  file_datas.size == 1 ? calculate_total_bytesize = file_datas.sum { |file_data| c_option(file_data) } : calculate_total_bytesize = c_option(file_datas)
-  determine_options_for_output_total_line(params_array, calculate_total_line, calculate_total_word, calculate_total_bytesize)
+  if params_array.empty?
+    [file_datas.sum { |file_data| count_line(file_data) }.to_s.rjust(8), file_datas.sum { |file_data| count_word(file_data) }.to_s.rjust(8), file_datas.sum { |file_data| count_bytesize(file_data) }.to_s.rjust(8)]
+  else
+    create_end_line(params_array,file_datas)
+  end
 end
 
 
@@ -89,10 +84,10 @@ def main
 
   if input_flag
     output_detail_line(files, file_datas, params_array)
-    puts display_total_line(file_datas) + "total" if files.size > 1
+    puts display_total_line(params_array, file_datas).join + "total" if files.size > 1
   else
     file_datas = $stdin.to_a.join
-    puts display_total_line(file_datas)
+    puts display_total_line(params_array, file_datas)
   end
 end
 
