19784136af2d7fdf488dbab564c0bde2cbcefc61
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-10-28 23:43:01 +0900

    指定したオプションを表すハッシュ名を変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -28,15 +28,15 @@ def count_bytesize(file_data)
   file_data.bytesize
 end
 
-def create_detail_line(params_array, file_data)
+def create_detail_line(options_array, file_data)
   number_of_line = count_line(file_data).to_s.rjust(8)
   nuber_of_word = count_word(file_data).to_s.rjust(8)
   nuber_of_bytesize = count_bytesize(file_data).to_s.rjust(8)
 
-  if params_array.empty?
+  if options_array.empty?
     [number_of_line, nuber_of_word, nuber_of_bytesize]
   else
-    params_array.map do |param|
+    options_array.map do |param|
       case param
       when 'l'
         number_of_line
@@ -50,23 +50,23 @@ def create_detail_line(params_array, file_data)
 end
 
 
-def display_detail_line(files, file_datas, params_array)
+def display_detail_line(files, file_datas, options_array)
   file_datas.each_with_index do |file_data, i|
     file_name = files[i]
-    detail_line = create_detail_line(params_array, file_data)
+    detail_line = create_detail_line(options_array, file_data)
     puts "#{detail_line.join} #{file_name}"
   end
 end
 
-def display_total_line(params_array, file_datas)
+def display_total_line(options_array, file_datas)
   total_number_of_lines = file_datas.sum { |file_data| count_line(file_data) }.to_s.rjust(8)
   total_number_of_words = file_datas.sum { |file_data| count_word(file_data) }.to_s.rjust(8)
   total_number_of_bytesize = file_datas.sum { |file_data| count_bytesize(file_data) }.to_s.rjust(8)
 
-  if params_array.empty?
+  if options_array.empty?
     [total_number_of_lines, total_number_of_words, total_number_of_bytesize]
   else
-    params_array.map do |param|
+    options_array.map do |param|
       case param
       when 'l'
         total_number_of_lines
@@ -81,19 +81,19 @@ end
 
 def main
   files = ARGV
-  params = define_options
+  options = define_options
   file_datas = files.map do |file|
     File.read(File.expand_path(file))
   end
 
-  params_array = params.keys.to_a.map(&:to_s).sort_by { |str| %w[l w c].index(str) }
+  options_array = options.keys.to_a.map(&:to_s).sort_by { |str| %w[l w c].index(str) }
 
   if stand_alone?
-    display_detail_line(files, file_datas, params_array)
-    puts display_total_line(params_array, file_datas).join + "total" if files.size > 1
+    display_detail_line(files, file_datas, options_array)
+    puts display_total_line(options_array, file_datas).join + "total" if files.size > 1
   else
     file_data = $stdin.to_a.join
-    puts create_detail_line(params_array, file_data).join
+    puts create_detail_line(options_array, file_data).join
   end
 end
 
