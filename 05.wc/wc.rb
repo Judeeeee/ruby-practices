a8b06681287cf120171cbc2e3dfd75587b738f29
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-10-30 20:48:11 +0900

    行出力の処理をメソッドにまとめた

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -40,15 +40,10 @@ def determine_caluculate(key, string_of_file)
 end
 
 # ファイルが複数指定された場合、合計を計算する
-def calculate_total(hash, total_hash)
+def calculate_total(hash, total_calculation_of_file_details)
   hash.each do |key, value|
-    total_hash[key] = total_hash[key] + hash[key]
+    total_calculation_of_file_details[key] = total_calculation_of_file_details[key] + hash[key]
   end
-end
-
-# 行の出力
-def display_detail_line(hash)
-  hash.map { |key, value| "#{value.to_s.rjust(8)}" }.join
 end
 
 # オプションを並び替える
@@ -56,9 +51,27 @@ def sort_options(hash)
   hash.sort_by { |str| %i[l w c].index(str[0]) }.to_h
 end
 
+# 行の出力(最後の行も含めて)
+def output_lines(file_details, file_name, total_calculation_of_file_details)
+  puts "#{display_detail_line(file_details)} #{file_name}"
+  calculate_total(file_details, total_calculation_of_file_details) if files.size != 1
+end
+
+# 行の出力
+def display_detail_line(hash)
+  hash.map { |key, value| "#{value.to_s.rjust(8)}" }.join
+end
+
+# オプション指定ありの行作成
+def create_detail_line(options, string_of_file)
+  options.each do |key, value|
+    options[key] = determine_caluculate(key, string_of_file)
+  end
+end
+
 def main
   options = define_options
-  total_hash = {l: 0, w: 0, c: 0}
+  total_calculation_of_file_details = {l: 0, w: 0, c: 0}
   files = ARGV
   if stand_alone?
     string_of_files = files.map do |file|
@@ -72,20 +85,16 @@ def main
     file_name = files[index]
     if options.empty?
       file_details = {l: count_line(string_of_file), w: count_word(string_of_file), c: count_bytesize(string_of_file)}
-      puts "#{display_detail_line(file_details)} #{file_name}"
-      calculate_total(file_details, total_hash) if files.size != 1
+      output_lines(file_details, file_name, total_calculation_of_file_details)
     else
-      options.each do |key, value|
-        options[key] = determine_caluculate(key, string_of_file)
-      end
+      create_detail_line(options, string_of_file)
       sorted_file_details = sort_options(options)
-      puts "#{display_detail_line(sorted_file_details)} #{file_name}"
-      calculate_total(options, total_hash) if files.size != 1
+      output_lines(sorted_file_details, file_name, total_calculation_of_file_details)
     end
   end
 
   if string_of_files.size != 1
-    sorted_file_details = sort_options(total_hash).delete_if{ |option, counted_detail| counted_detail.nil? }
+    sorted_file_details = sort_options(total_calculation_of_file_details).delete_if{ |option, counted_detail| counted_detail.nil? }
     puts "#{ display_detail_line(sorted_file_details) } total"
   end
 end
