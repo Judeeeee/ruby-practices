3eeda8dc6111820d622bd6f729e61899010177d9
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-10-30 20:04:27 +0900

    処理を追いづらいので、コメントを削除

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -70,33 +70,21 @@ def main
 
   file_datas.each_with_index do |file_data, index|
     file_name = files[index]
-    # オプションの有無判定
     if options.empty?
-      # オプションが指定されない場合
       file_details = {l: count_line(file_data), w: count_word(file_data), c: count_bytesize(file_data)}
-
       puts "#{display_detail_line(file_details)} #{file_name}"
-
-      # オプションが指定されない場合でファイルが複数指定された場合
       calcllate_total(file_details, total_hash) if files.size != 1
-
     else
-      # オプションが指定された場合
       options.each do |key, value|
         options[key] = determine_caluculate(key, file_data)
       end
-
       foo = sort_options(options)
       puts "#{display_detail_line(foo)} #{file_name}"
-
-      # オプションが指定された場合でファイルが複数指定された場合
       calcllate_total(options, total_hash) if files.size != 1
     end
   end
 
-  # ファイルが複数指定された場合は、"total"の行を出力する
   if file_datas.size != 1
-    # 並び替える
     foo = sort_options(total_hash).delete_if{ |key, value| value == 0 }
     puts "#{ display_detail_line(foo) } total"
   end
