fc44d13bcc04b47b453be423bc650fd950341da9
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-09 11:33:36 +0900

    RuboCopの指摘修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,11 +3,11 @@
 require 'optparse'
 
 def create_detail_line(options, string_of_file)
-  detail_line = Hash.new
+  detail_line = {}
   options.each do |option, _selected_option|
     detail_line[option] = determine_caluculate(option, string_of_file)
   end
-  detail_line.reject{|_option, counted_detail| counted_detail.zero?}
+  detail_line.reject { |_option, counted_detail| counted_detail.zero? }
 end
 
 def determine_caluculate(option, string_of_file)
@@ -34,7 +34,7 @@ def count_bytesize(string_of_file)
 end
 
 def output_total_line(file_details_total)
-  total_line = file_details_total.reject{|_option, counted_detail| counted_detail.zero? }
+  total_line = file_details_total.reject { |_option, counted_detail| counted_detail.zero? }
   puts "#{output_lines(total_line)} total"
 end
 
@@ -44,7 +44,7 @@ end
 
 def define_options
   opt = OptionParser.new
-  params = {l: nil, w: nil, c: nil}
+  params = { l: nil, w: nil, c: nil }
   opt.on('-l') { |v| params[:l] = v }
   opt.on('-w') { |v| params[:w] = v }
   opt.on('-c') { |v| params[:c] = v }
@@ -61,9 +61,8 @@ def stand_alone?
 end
 
 def inilialize_file_details_total(options)
-
+  file_details_total = {}
   options.each_key do |option|
-    file_details_total = Hash.new
     file_details_total[option] = 0
   end
 end
@@ -73,23 +72,24 @@ def main
 
   if stand_alone?
     files = ARGV
-    file_details = files.to_h {|file| [file, File.read(File.expand_path(file))]}
+    file_details = files.to_h { |file| [file, File.read(File.expand_path(file))] }
     file_details_total = inilialize_file_details_total(options)
 
     file_details.each do |file_name, file_string|
       detail_line = create_detail_line(options, file_string)
       puts "#{output_lines(detail_line)} #{file_name}"
 
-      if file_details.keys.size != 1
-        options.each_key do |option|
-          file_details_total[option] += detail_line[option]
-        end
+      next if file_details.keys.size != 1
+
+      options.each_key do |option|
+        file_details_total[option] += detail_line[option]
       end
     end
+
     output_total_line(file_details_total) if file_details.size != 1
   else
     detail_line = create_detail_line(options, $stdin.to_a.join)
-    puts "#{output_lines(detail_line)}"
+    puts output_lines(detail_line)
   end
 end
 
