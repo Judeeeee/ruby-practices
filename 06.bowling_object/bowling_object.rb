63679d964605f6d2c69d74b92eb55bfcb03a04f0
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-13 17:23:08 +0900

    大枠のロジックを定めた

--- 06.bowling_object/bowling_object.rb
+++ 06.bowling_object/bowling_object.rb
@@ -1,54 +1,82 @@
 # frozen_string_literal: true
 
-def main
-  scoreboard = ARGV[0]
-  frames = split_by_frames(scoreboard)
-  puts calculate_score(frames)
-end
-
-def strike?(frame)
-  frame == [10, 0]
-end
-
-def spare?(frame)
-  !strike?(frame) && (frame[0..1].sum == 10)
-end
-
-def split_by_frames(scoreboard)
-  scores = scoreboard.split(',')
-
-  shots = scores.flat_map do |score|
-    score == 'X' ? [10, 0] : score.to_i
+class Shot
+  def initialize(mark)
+    @mark = mark
   end
 
-  shots.each_slice(2).to_a
+  def x_to_10
+    @mark == 'X' ? [10, 0] : @mark.to_i
+  end
 end
 
-def caluculate_bonus_score(frames, index)
-  frame = frames[index]
-  next_frame = frames[index + 1]
 
-  if spare?(frame)
-    next_frame[0]
-  elsif strike?(frame)
-    if strike?(next_frame)
-      after_next_frame = frames[index + 2]
-      next_frame[0] + after_next_frame[0]
-    else
-      next_frame[0..1].sum
+class Frame
+  def initialize(shots)
+    @shots = shots
+  end
+
+  def split_by_frames
+    @shots.flatten.each.each_slice(2).to_a
+  end
+end
+
+
+class Game
+  def initialize(frames)
+    @frames = frames
+  end
+
+  def calculate_score
+    frame_score = @frames[0..8].flatten.sum + @frames[9..].flatten.sum
+    bonus_score = caluculate_bonus_score
+    frame_score + bonus_score
+    binding.irb
+  end
+
+  private
+
+  def caluculate_bonus_score
+    bonus_result = 0
+    @frames.each_with_index do |frame, index|
+      next_frame = @frames[index + 1]
+
+      if spare?(frame)
+        bonus_result += next_frame[0]
+      elsif strike?(frame)
+        if strike?(next_frame)
+          after_next_frame = @frames[index + 2]
+          bonus_result += next_frame[0] + after_next_frame[0]
+        else
+          bonus_result += next_frame[0..1].sum
+        end
+      else
+        bonus_result += 0
+      end
     end
-  else
-    0
+    bonus_result
+  end
+
+  def strike?(frame)
+    frame == [10, 0]
+  end
+
+  def spare?(frame)
+    !strike?(frame) && (frame[0..1].sum == 10)
   end
 end
 
-def calculate_score(frames)
-  up_to_9th_total = frames[0..8].each_with_index.sum do |frame, index|
-    frame.sum + caluculate_bonus_score(frames, index)
+def main
+  scoreboard = ARGV[0].split(',')
+  shots = []
+  scoreboard.each do |mark|
+    shots << Shot.new(mark).x_to_10
   end
 
-  last_frame = frames[9..].flatten
-  up_to_9th_total + last_frame.sum
+  frames = Frame.new(shots).split_by_frames
+  total_result = Game.new(frames)
+  game_score = total_result.calculate_score
+  puts game_score
 end
 
 main if __FILE__ == $PROGRAM_NAME
