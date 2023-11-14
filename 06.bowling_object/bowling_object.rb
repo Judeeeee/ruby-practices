b972b7f02afc6e480ef274ec271cdda379ea0c0e
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-14 16:04:03 +0900

    ボーナススコアを計算する記述が長いので、メソッドに切り出した

--- 06.bowling_object/bowling_object.rb
+++ 06.bowling_object/bowling_object.rb
@@ -38,26 +38,30 @@ def main
   base_score = x_converted_to_10_points.sum
   shots = x_converted_to_10_points.each_slice(2).to_a
   frames = shots.map { |shot| Frame.new(shot) }
-  bonus_score = 0
+  bonus_score = calculate_bonus_score(frames)
+  game_score = base_score + bonus_score
 
+  puts game_score
+end
+
+def calculate_bonus_score(frames)
+  bonus_score = 0
   frames.each_with_index do |frame, index|
-    break if index == 9 #最後のフレームではボーナススコアを計算しないため
+    break if index == 9 # 最後のフレームではボーナススコアを計算しないため
+
     next_frame = frames[index + 1]
     after_next_frame = frames[index + 2]
 
     if frame.spare?
       bonus_score += next_frame.first_shot
     elsif frame.strike?
-      if next_frame.strike?
-        bonus_score += next_frame.first_shot + after_next_frame.first_shot
-      else
-        bonus_score += next_frame.total_shot
-      end
+      bonus_score += if next_frame.strike?
+                       next_frame.first_shot + after_next_frame.first_shot
+                     else
+                       next_frame.total_shot
+                     end
     end
   end
-
-  game_score = base_score + bonus_score
-  puts game_score
 end
 
 main if __FILE__ == $PROGRAM_NAME
