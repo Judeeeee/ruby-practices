52c64e0f30555f75df759ab9b05b967f3d65fd34
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-14 15:52:54 +0900

    何パターンかで計算結果が期待通りに出力されることを確認した

--- 06.bowling_object/bowling_object.rb
+++ 06.bowling_object/bowling_object.rb
@@ -1,80 +1,62 @@
 # frozen_string_literal: true
 
 class Shot
-  def initialize(mark)
-    @mark = mark
+  def initialize(point)
+    @point = point
   end
 
   def x_to_10
-    @mark == 'X' ? [10, 0] : @mark.to_i
+    @point == 'X' ? [10, 0] : @point.to_i
   end
 end
-
 
 class Frame
   def initialize(shots)
     @shots = shots
   end
 
-  def split_by_frames
-    @shots.flatten.each.each_slice(2).to_a
-  end
-end
-
-
-class Game
-  def initialize(frames)
-    @frames = frames
+  def strike?
+    @shots == [10, 0]
   end
 
-  def calculate_score
-    frame_score = @frames[0..8].flatten.sum + @frames[9..].flatten.sum
-    bonus_score = caluculate_bonus_score
-    frame_score + bonus_score
+  def spare?
+    !strike? && (@shots.sum == 10)
   end
 
-  private
-
-  def caluculate_bonus_score
-    bonus_result = 0
-    @frames[0..8].each_with_index do |frame, index|
-      next_frame = @frames[index + 1]
-
-      if spare?(frame)
-        bonus_result += next_frame[0]
-      elsif strike?(frame)
-        if strike?(next_frame)
-          after_next_frame = @frames[index + 2]
-          bonus_result += next_frame[0] + after_next_frame[0]
-        else
-          bonus_result += next_frame[0..1].sum
-        end
-      else
-        bonus_result += 0
-      end
-    end
-    bonus_result
+  def first_shot
+    @shots[0]
   end
 
-  def strike?(frame)
-    frame == [10, 0]
-  end
-
-  def spare?(frame)
-    !strike?(frame) && (frame[0..1].sum == 10)
+  def total_shot
+    @shots.sum
   end
 end
 
 def main
-  scoreboard = ARGV[0].split(',')
-  shots = []
-  scoreboard.each do |mark|
-    shots << Shot.new(mark).x_to_10
+  points = ARGV[0].split(',')
+  x_converted_to_10_points = points.map { |point| Shot.new(point).x_to_10 }.flatten
+  base_score = x_converted_to_10_points.sum
+  shots = x_converted_to_10_points.each_slice(2).to_a
+  frames = shots.map { |shot| Frame.new(shot) }
+  bonus_score = 0
+
+  frames.each_with_index do |frame, index|
+    break if index == 9 #最後のフレームではボーナススコアを計算しないため
+    next_frame = frames[index + 1]
+    after_next_frame = frames[index + 2]
+
+    if frame.spare?
+      bonus_score += next_frame.first_shot
+    elsif frame.strike?
+      if next_frame.strike?
+        bonus_score += next_frame.first_shot + after_next_frame.first_shot
+      else
+        bonus_score += next_frame.total_shot
+      end
+    end
   end
 
-  frames = Frame.new(shots).split_by_frames
-  total_result = Game.new(frames)
-  game_score = total_result.calculate_score
+  game_score = base_score + bonus_score
   puts game_score
 end
 
